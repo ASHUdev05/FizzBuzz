cb2eab27a32ad3114ec867363a94bb2f5c4fbe42
ASHUTOSH KUMAR JHA <84619413+ASHUdev05@users.noreply.github.com>
2021-11-20 06:35:27 +0000

    Added solution in rust

---
+++ fizzbuzz.rs
@@ -0,0 +1,18 @@
+fn fizzbuzz(n: i32){
+    for i in 1..n+1{
+        if i%3==0 && i%5==0{
+            println!("FizzBuzz");
+        }else if i%3==0{
+            println!("Fizz");
+        }else if i%5==0{
+            println!("Buzz");
+        }else{
+            println!("{}", i);
+        }
+    }
+}
+
+fn main(){
+    let n=20;
+    fizzbuzz(n);
+}
